942e83b7a6e7568bec9450fe923385a1539ae888
Siddhant <84025779+methlox@users.noreply.github.com>
2023-02-12 01:58:46 +0530

    03 post req

--- src/index.js
+++ src/index.js
@@ -1,30 +1,40 @@
 // console.log("hello")
 
+const { request } = require('express');
 const express = require('express');  // imports express library
 
 const app = express(); // creates an instance application of express
 
 const PORT = 3001; // we need to set up a port to start listening to requests coming in
 
+app.use(express.json()); // middleware
+app.use(express.urlencoded());
+
 app.listen(PORT, () => console.log(`express server running on PORT ${PORT}`));
 
+const groceryList = [
+    {
+        item: "milk",
+        quantity: 2
+    },
+    {
+        item: "apples",
+        quantity: 4
+    },
+    {
+        item: "pop-tarts",
+        quantity: 4
+    },
+];
+
+// GET REQ
 app.get('/groceries', (req, res) => {
-    res.send([
-        {
-            item: "milk",
-            quantity: 2
-        },
-
-        {
-            item: "apples",
-            quantity: 4
-        },
-
-        {
-            item: "pop-tarts",
-            quantity: 4
-        },
-
-
-    ])
+    res.send(groceryList);
 });
+
+// POST REQ
+app.post('/groceries', (req, res) => {
+    console.log(req.body);
+    groceryList.push(req.body);
+    res.sendStatus(201);
+})
