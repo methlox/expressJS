a43b1497e86c65d66148d65c9e93690d34d84c85
Siddhant <84025779+methlox@users.noreply.github.com>
2023-02-12 02:26:15 +0530

    04 middleware 2

--- src/index.js
+++ src/index.js
@@ -8,7 +8,12 @@ const app = express(); // creates an instance application of express
 const PORT = 3001; // we need to set up a port to start listening to requests coming in
 
 app.use(express.json()); // middleware
-app.use(express.urlencoded());
+app.use(express.urlencoded({extended: true}));
+app.use((req, res, next) => {
+    // console.log(req.url);
+    console.log(`${req.method}:${req.url}`);
+    next();
+});
 
 app.listen(PORT, () => console.log(`express server running on PORT ${PORT}`));
 
@@ -28,10 +33,7 @@ const groceryList = [
 ];
 
 // GET REQ
-app.get('/groceries',(req, res, next) => {
-    console.log('Before handling the request');
-    next();
-}, (req, res) => {
+app.get('/groceries', (req, res) => {
     res.send(groceryList);
 });
 
