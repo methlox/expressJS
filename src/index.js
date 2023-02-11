c9d74385f14237139443b7ec1c9a79c0fd22a3e1
Siddhant <84025779+methlox@users.noreply.github.com>
2023-02-12 01:02:59 +0530

    02 get req

--- src/index.js
+++ src/index.js
@@ -7,3 +7,24 @@ const app = express(); // creates an instance application of express
 const PORT = 3001; // we need to set up a port to start listening to requests coming in
 
 app.listen(PORT, () => console.log(`express server running on PORT ${PORT}`));
+
+app.get('/groceries', (req, res) => {
+    res.send([
+        {
+            item: "milk",
+            quantity: 2
+        },
+
+        {
+            item: "apples",
+            quantity: 4
+        },
+
+        {
+            item: "pop-tarts",
+            quantity: 4
+        },
+
+
+    ])
+});
