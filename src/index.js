c7ed9cd990f8b75fc071129affc67a330f81e7f3
Siddhant <84025779+methlox@users.noreply.github.com>
2023-02-12 00:45:30 +0530

    01 intro and project setup

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// console.log("hello")
+
+const express = require('express');  // imports express library
+
+const app = express(); // creates an instance application of express
+
+const PORT = 3001; // we need to set up a port to start listening to requests coming in
+
+app.listen(PORT, () => console.log(`express server running on PORT ${PORT}`));
